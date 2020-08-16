cc1a4d9d3f5cc0edf16eb26712a62bb88d104f06
Ghazi Khan <ghazikhan205@gmail.com>
2020-08-17 01:42:33 +0530

    updated Readme file

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,39 @@
-import React from 'react'
+import React, { Component, createRef } from 'react'
 
 import Form from 'formify-react'
 import 'formify-react/dist/index.css'
 
 import { formConstants } from './config'
 
-const App = () => {
-  return <Form model={formConstants} />
-}
+const budgetData = [
+  { value: 5000, label: '$5000' },
+  { value: 15000, label: '$15000' },
+  { value: 25000, label: '$25000' },
+  { value: 35000, label: '$35000' },
+  { value: 50000, label: '$50000' }
+]
 
-export default App
+export default class App extends Component {
+  formRef = createRef()
+
+  handleSave = () => {
+    const { current } = this.formRef
+    const form = current.getFormData()
+    if (form.isFormValid) {
+      console.log(form.formData)
+    }
+  }
+
+  render() {
+    return (
+      <div className='container'>
+        <Form
+          model={formConstants}
+          ref={this.formRef}
+          data={{ budget: budgetData }}
+        />
+        <button onClick={this.handleSave}>Save</button>
+      </div>
+    )
+  }
+}
