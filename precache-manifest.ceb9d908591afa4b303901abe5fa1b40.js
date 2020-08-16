2d765d62e8fab8d1190b92ffeac1a76cb7e97203
Khan Ghazi <Ghazi.Khan@avaloq.com>
2020-08-17 01:08:22 +0530

    Updates

---
+++ precache-manifest.ceb9d908591afa4b303901abe5fa1b40.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e49a54d5d4ff26d72b9c6f1da0a605b8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ecf66d98e77be4405f74",
+    "url": "./static/css/main.8852e414.chunk.css"
+  },
+  {
+    "revision": "d36edcc788c995904b58",
+    "url": "./static/js/2.99baa5b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.99baa5b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecf66d98e77be4405f74",
+    "url": "./static/js/main.ca0fe282.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.ca0fe282.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15162f302a8e2a015277",
+    "url": "./static/js/runtime-main.74342f79.js"
+  }
+]);
